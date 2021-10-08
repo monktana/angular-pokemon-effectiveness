44048f39007e6e57c893e079fcccb76ba2d9bf9e
monktana <svengiebelmann@gmail.com>
2021-10-08 16:36:17 +0200

    saves value of observable for direct(er) access

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { PokemonService } from "./../pokemon/pokemon.service";
+import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Pokemon } from '../pokemon/pokemon';
+import { PokemonService } from "../pokemon/pokemon.service";
 
 @Component({
   selector: 'app-main',
@@ -9,13 +9,24 @@ import { Pokemon } from '../pokemon/pokemon';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-  pokemon: Observable<Pokemon[]>;
+  pokemon$!: Observable<Pokemon[]>;
+  pokemon!: Pokemon[];
 
-  constructor(private pokemonService: PokemonService) { 
-    this.pokemon = this.pokemonService.getRandomPokemon(2);
+  constructor(private pokemonService: PokemonService) { }
+
+  ngOnInit() {
+    this.refreshPokemon()
   }
 
-  ngOnInit() { }
-
   ngOnDestroy() { }
+
+  private refreshPokemon(): void {
+    this.pokemon$ = this.pokemonService.getRandomPokemon(2);
+    var subscription = this.pokemon$.subscribe((value: Pokemon[]) => this.pokemon = value);
+    subscription.unsubscribe();
+  }
+
+  private fight(): number {
+    return 0;
+  }
 }
