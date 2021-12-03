352a440f7875daff404b9f31e877f1333ea290fd
monktana <svengiebelmann@gmail.com>
2021-12-03 17:08:05 +0100

    fixed(test): fixed component creation tests

--- src/app/game/game.component.spec.ts
+++ src/app/game/game.component.spec.ts
@@ -1,4 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Pokemon } from '../pokemon/pokemon';
+import { PokeapiService } from '../pokemon/services/pokeapi.service';
+import { LocalStorageScoreService } from '../score/services/local-storage-score.service';
+import { TemporaryScoreService } from '../score/services/temporary-score.service';
 
 import { GameComponent } from './game.component';
 
@@ -6,20 +10,38 @@ describe('GameComponent', () => {
   let component: GameComponent;
   let fixture: ComponentFixture<GameComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ GameComponent ]
-    })
-    .compileComponents();
-  });
+  let pokeApiServiceStub: Partial<PokeapiService>;
+  let temporaryScoreServiceStub: Partial<TemporaryScoreService>;
+  let localStorageServiceStub: Partial<LocalStorageScoreService>;
+
+  const attacking: Pokemon = {name:'charmandar', sprites: {back_default: '', back_shiny: '', front_default: '', front_shiny: ''}, types: ['fire']};
+  const defending: Pokemon = {name:'blubasaur', sprites: {back_default: '', back_shiny: '', front_default: '', front_shiny: ''}, types: ['grass', 'poison']};
+
+  beforeEach(waitForAsync(() => {
+    pokeApiServiceStub = {};
+    temporaryScoreServiceStub = {};
+    localStorageServiceStub = {};
+
+    TestBed.configureTestingModule({
+      declarations: [ GameComponent ],
+      providers: [ 
+        {provide: PokeapiService, useValue: pokeApiServiceStub},
+        {provide: TemporaryScoreService, useValue: temporaryScoreServiceStub},
+        {provide: LocalStorageScoreService, useValue: localStorageServiceStub}
+      ]
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(GameComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    component.attacking = attacking;
+    component.defending = defending;
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeDefined();
   });
 });
--- src/app/main/main.component.spec.ts
+++ src/app/main/main.component.spec.ts
@@ -1,19 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { PokeapiService } from '../pokemon/services/pokeapi.service';
-
 import { MainComponent } from './main.component';
 
 describe('MainComponent', () => {
   let component: MainComponent;
   let fixture: ComponentFixture<MainComponent>;
-  let pokeApiServiceStub: Partial<PokeapiService>;
 
   beforeEach(async () => {
-    pokeApiServiceStub = {};
-
     TestBed.configureTestingModule({
-      declarations: [ MainComponent ],
-      providers: [ {provide: PokeapiService, useValue: pokeApiServiceStub} ]
+      declarations: [ MainComponent ]
     });
     fixture = TestBed.createComponent(MainComponent);
     component = fixture.componentInstance;
