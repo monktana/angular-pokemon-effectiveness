578e0e3025796ecb3c03bfb824ad26cd615dfc9d
Sven Giebelmann <sven@giebelmann.dev>
2022-10-29 19:49:33 +0200

    test: add input dependecy to component

--- src/app/round/__tests__/round.component.spec.ts
+++ src/app/round/__tests__/round.component.spec.ts
@@ -1,10 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Matchup } from 'src/app/matchup/matchup';
 
 import { RoundComponent } from '../round.component';
+
+import POKEMON_FIXTURE from '../../../testing/fixtures/pokemon.json';
+import MOVE_FIXTURE from '../../../testing/fixtures/moves.json';
 
 describe('RoundComponent', () => {
   let component: RoundComponent;
   let fixture: ComponentFixture<RoundComponent>;
+  let matchup: Matchup;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -13,6 +18,20 @@ describe('RoundComponent', () => {
 
     fixture = TestBed.createComponent(RoundComponent);
     component = fixture.componentInstance;
+
+    const attacker = POKEMON_FIXTURE.find(
+      pokemon => pokemon.name === 'bulbasaur'
+    )!;
+    const move = MOVE_FIXTURE.find(move => move.name === 'tackle')!;
+    const defender = POKEMON_FIXTURE.find(
+      pokemon => pokemon.name === 'squirtle'
+    )!;
+
+    component.matchup = {
+      attacker: { ...attacker, move },
+      defender,
+    };
+
     fixture.detectChanges();
   });
 
